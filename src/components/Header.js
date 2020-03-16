31886dd43615bae15ea27155081705f17841ff87
Tripti Saini <triptisaini123@gmail.com>
2020-03-17 01:27:52 +0530

    Added login feature, with temporary UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Logout from './Logout';
 
 class Header extends Component {
 
     state = {
         collapseOnSelect: true,
         defaultExpanded: true
-    }
+    };
+
+    static propTypes = {
+        auth: PropTypes.object.isRequired
+    };
 
     render() {
+
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <Fragment>
+                <Logout></Logout>
+            </Fragment>
+        );
+
+        const guestLinks = (
+            <Fragment>
+                <NavDropdown title="User" id="collasible-nav-dropdown-user">
+                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                    <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+                </NavDropdown>
+            </Fragment>
+        );
+
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand href="/">TNP Portal IGDTUW</Navbar.Brand>
@@ -17,10 +44,7 @@ class Header extends Component {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <NavDropdown title="User" id="collasible-nav-dropdown-user">
-                    <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                    <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-                </NavDropdown>
+                { isAuthenticated ? authLinks : guestLinks }
                 <NavDropdown title="About Us" id="collasible-nav-dropdown-about-us">
                     <NavDropdown.Item href="/history">History</NavDropdown.Item>
                     <NavDropdown.Item href="/alumni">Alumni</NavDropdown.Item>
@@ -41,4 +65,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(Header);
