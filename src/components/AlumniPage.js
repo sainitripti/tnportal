d7ee9d1e04eb9c08e43c0d8ef3184524af22a785
Tripti Saini <triptisaini123@gmail.com>
2020-04-14 16:15:04 +0530

    updated alumni page

--- src/components/AlumniPage.js
+++ src/components/AlumniPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-
+import Carousel from 'react-bootstrap/Carousel'
+import alumni1 from "../images/DurgaShaktiNagpal.png";
 class AlumniPage extends Component {
     render() {
+        
         return (
             <div>
-                <p>Welcome to AlumniPage Component!!</p>
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={alumni1}
+                    alt="First slide- Durga shakti nagpal, IAS"
+                  />
+                </Carousel.Item>
+              </Carousel>
             </div>
         )
     }
